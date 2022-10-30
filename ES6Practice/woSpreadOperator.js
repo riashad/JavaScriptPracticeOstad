6626451e238afec6421983fd3eaaff9687d3a5d9
Riashad Noor <riashad.noor@gmail.com>
2022-10-30 20:35:30 +0600

    second commit

--- ES6Practice/woSpreadOperator.js
+++ ES6Practice/woSpreadOperator.js
@@ -1,6 +1,7 @@
 "use strict"
 let mernStack = ['MongoDB', 'Express', 'React', 'Node']
 let meanStack = ['MongoDB', 'Express', 'Angular', 'Node']
-console.log(meanStack +" "+ mernStack)
+console.log(meanStack +" "+ mernStack) //concat
 
 console.log(mernStack.push())
+mernStack.push(meanStack)

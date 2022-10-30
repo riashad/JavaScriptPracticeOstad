72c35954e3fc4d648e541d8e615ca9e0ecc00f20
Riashad Noor <riashad.noor@gmail.com>
2022-10-30 23:29:02 +0600

    Dynamic function

---
+++ ES6Practice/dynamicFunc.js
@@ -0,0 +1,12 @@
+//Dynamic function don't have function name, rather variable is used to assign and call the function.
+
+var status = function (myStatus) {
+    return myStatus;
+}
+console.log(status("Learning...not gonna give up!\n"));
+
+var profile = function (myName, myAge, myCountry, myEducation) {
+    return "My Name: " + myName +"\n"+"My Age:" +myAge + "\n" + "Country: " + myCountry + "\n" + "Education: " + myEducation;
+}
+
+console.log(profile("Riashad Noor", "31", "Bangladesh", "BSc. in CSE"));

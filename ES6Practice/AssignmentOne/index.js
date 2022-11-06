70fb5261f866d0e885279fc85710475ae7efc130
Riashad Noor <riashad.noor@gmail.com>
2022-11-06 10:49:18 +0600

    Assignment 1 - Completed

---
+++ ES6Practice/AssignmentOne/index.js
@@ -0,0 +1,7 @@
+import {districtList} from "./districtList.js"; // Named Module Import
+
+districtList.map((currentItem, index) => {
+	let distName = "My district name is: " + currentItem.name;
+	let distCode = `My district code is ${currentItem.code}`;//Template strings use Tick (`) and it helps to include variables inside the string
+	console.log(distName + "\n" + distCode);
+});
---
+++ ES6Practice/AssignmentOne/readme.md
@@ -0,0 +1,11 @@
+**Assignment Tasks:**
+
+1. Create a Array list
+2. Export the Array List
+3. Import the Array List
+4. Iterate the values of the Array using Map
+
+Generate Package.json file
+Inside package.json file add "type": "module" to keep all the files inside the module
+for import and export
+Check the changes automatically using nodemon package.

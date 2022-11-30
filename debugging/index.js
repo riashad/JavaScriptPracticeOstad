37d22b284c9476a7c421bf4bf2b14dc3f9139ac6
Riashad Noor <riashad.noor@gmail.com>
2022-11-30 19:42:29 +0600

    Debugging using IDE Dubgging tools

---
+++ debugging/index.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 40;
+let e = "50";
+let f = 60;
+let g = 90;
+
+let ab = a+b+c ;
+let bc = (d+e+f)*g;
+
+let result = bc - ab;
+console.log(result);
---
+++ debugging/index2.js
@@ -0,0 +1,19 @@
+let axios=require('axios');
+
+let URL="https://restcountries.eu/rest/v2/all";
+
+
+
+axios.get(URL).then(function (response) {
+
+
+	let CountryList=response.data;
+	let StatusCode=response.status;
+
+	console.log(CountryList)
+	console.log(StatusCode)
+
+
+}).catch(function (error){
+	console.log(error)
+})
